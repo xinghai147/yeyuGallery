b90f85dcd5c243c1f0e34e25c610b26f17655ea2
星海 <386994864@qq.com>
2025-03-14 18:27:45 +0800

    图库前端

---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="basiclayout">
+    <a-layout style="min-height: 100vh">
+      <a-layout-header class="header"><GlobalHeader /></a-layout-header>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer"> 云图库学习平台 </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<script setup lang="ts">
+import GlobalHeader from '@/components/GlobalHeader.vue'
+</script>
+
+<style scoped>
+#basiclayout .footer {
+  background: #efefef;
+  padding: 16px;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+#basiclayout .content {
+  padding: 20px;
+  background: linear-gradient(to bottom, #f0f0f0, #ffffff);
+  margin-bottom: 28px;
+}
+</style>
