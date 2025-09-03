6d9b54b4886fac4fdd5d1ba291fc4daee22c495e
星海 <386994864@qq.com>
2025-09-03 23:43:08 +0800

    云图库后端

---
+++ src/pages/AddPicturePage.vue
@@ -0,0 +1,151 @@
+<template>
+  <div id="addPicturePage">
+    <h2 style="margin-bottom: 16px">
+      {{ route.query?.id ? '修改图片' : '创建图片' }}
+    </h2>
+    <!--图片上传组件-->
+    <PictureUpload :picture="picture" :onSuccess="onSuccess" />
+    <!--图片信息表单-->
+    <a-form
+      v-if="picture"
+      name="pictureForm"
+      :label-col="{ span: 4 }"
+      :wrapper-col="{ span: 14 }"
+      yout="vertical"
+      :model="pictureForm"
+      @finish="handleSubnit"
+    >
+      <a-form-item name="name" label="名称">
+        <a-input v-model:value="pictureForm.name" placeholder="请输入图片名称" allow-clear />
+      </a-form-item>
+      <a-form-item name="introduction" label="简介">
+        <a-textarea
+          v-model:value="pictureForm.introduction"
+          placeholder="请输入简介"
+          :auto-size="{ minRows: 2, maxRows: 5 }"
+          allow-clear
+        />
+      </a-form-item>
+      <a-form-item name="category" label="分类">
+        <a-auto-complete
+          v-model:value="pictureForm.category"
+          placeholder="请输入分类"
+          :options="categoryOptions"
+          allow-clear
+        />
+      </a-form-item>
+      <a-form-item name="tags" label="标签">
+        <a-select
+          v-model:value="pictureForm.tags"
+          mode="tags"
+          placeholder="请输入标签"
+          :options="tagOptions"
+          allow-clear
+        />
+      </a-form-item>
+      <a-form-item>
+        <a-button type="primary" html-type="submit" style="width: 100%">创建</a-button>
+      </a-form-item>
+    </a-form>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {
+  editPictureUsingPost,
+  getPictureVoByIdUsingGet,
+  listPictureTagCategoryUsingGet,
+} from '@/api/pictureController'
+import PictureUpload from '@/components/PictureUpload.vue'
+import { message } from 'ant-design-vue'
+import { onMounted, reactive, ref } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+
+const picture = ref<API.PictureVO>()
+
+const pictureForm = reactive<API.PictureEditRequest>({})
+
+const router = useRouter()
+// 图片上传成功
+const onSuccess = (newPicture: API.PictureVO) => {
+  picture.value = newPicture
+  pictureForm.name = newPicture.name
+}
+
+//处理表单提交
+const handleSubnit = async (values: any) => {
+  const pictureId = picture.value?.id
+  if (!pictureId) {
+    return message.error('请先上传图片')
+  }
+  const res = await editPictureUsingPost({
+    id: pictureId,
+    ...values,
+  })
+  //操作成功
+  if (res.data.code === 0 && res.data.data) {
+    message.success('创建成功')
+    router.push({ path: `/picture/${pictureId}` })
+    // 跳转到详情页
+  } else {
+    message.error('创建失败' + res.data.message)
+  }
+}
+
+const categoryOptions = ref<string[]>([])
+
+const tagOptions = ref<string[]>([])
+
+// 获取标签和分类选项
+const getTagCategoryOpitions = async () => {
+  const res = await listPictureTagCategoryUsingGet()
+  if (res.data.code === 0 && res.data.data) {
+    tagOptions.value = (res.data.data.tagList ?? []).map((data: string) => ({
+      label: data,
+      value: data,
+    }))
+    categoryOptions.value = (res.data.data.categoryList ?? []).map((data: string) => ({
+      label: data,
+      value: data,
+    }))
+  } else {
+    message.error('获取标签列表失败' + res.data.message)
+  }
+}
+
+onMounted(() => {
+  getTagCategoryOpitions()
+})
+
+const route = useRoute()
+
+// 获取旧数据
+const getOldPicture = async () => {
+  // 获取数据id
+  const id = route.query?.id
+  if (id) {
+    const res = await getPictureVoByIdUsingGet({
+      id: id,
+    })
+    if (res.data.code === 0 && res.data.data) {
+      const data = res.data.data
+      picture.value = data
+      pictureForm.name = data.name
+      pictureForm.introduction = data.introduction
+      pictureForm.category = data.category
+      pictureForm.tags = data.tags
+    }
+  }
+}
+
+onMounted(() => {
+  getOldPicture()
+})
+</script>
+
+<style scoped>
+#addPicturePage {
+  max-width: 720px;
+  margin: 0 auto;
+}
+</style>
---
+++ src/pages/admin/PictureManagePage.vue
@@ -0,0 +1,259 @@
+<template>
+  <div id="pictureManagePage">
+    <!--搜索表单-->
+    <a-form layout="inline" :model="searchParams" @finish="doSearch">
+      <a-form-item label="关键词">
+        <a-input
+          v-model:value="searchParams.searchText"
+          placeholder="从名称和简介搜索"
+          allow-clear
+        />
+      </a-form-item>
+      <a-form-item label="类型">
+        <a-input v-model:value="searchParams.category" placeholder="请输入类型" allow-clear />
+      </a-form-item>
+      <a-form-item label="标签">
+        <a-select
+          v-model:value="searchParams.tags"
+          mode="tags"
+          placeholder="请输入标签"
+          style="min-width: 180px"
+          allow-clear
+        />
+      </a-form-item>
+      <a-form-item label="审核状态" name="reviewStatus">
+        <a-select
+          v-model:value="searchParams.reviewStatus"
+          :options="PIC_REVIEW_STATUS_OPTIONS"
+          placeholder="请选择审核状态"
+          style="min-width: 180px"
+          allow-clear
+        />
+      </a-form-item>
+      <a-form-item>
+        <a-button type="primary" html-type="submit">搜索</a-button>
+      </a-form-item>
+    </a-form>
+    <div style="margin-bottom: 14px"></div>
+    <!--表格-->
+    <a-table
+      :columns="columns"
+      :data-source="dataList"
+      :pagination="pagination"
+      @change="doTabChange"
+      :scroll="{ x: 'max-content' }"
+    >
+      <template #bodyCell="{ column, record }">
+        <template v-if="column.dataIndex === 'url'">
+          <a-image :src="record.url" :width="120"></a-image>
+        </template>
+        <template v-if="column.dataIndex === 'tags'">
+          <a-space wrap>
+            <a-tag v-for="tag in JSON.parse(record.tags || '[]')" :key="tag">
+              {{ tag }}
+            </a-tag>
+          </a-space>
+        </template>
+        <template v-if="column.dataIndex === 'picInfo'">
+          <div>格式：{{ record.picFormat }}</div>
+          <div>宽度：{{ record.picWidth }}</div>
+          <div>高度：{{ record.picHeight }}</div>
+          <div>宽高比：{{ record.picScale }}</div>
+          <div>大小：{{ (record.picSize / 1024).toFixed(2) }}KB</div>
+        </template>
+        <template v-if="column.dataIndex === 'reviewMessage'">
+          <div>审核状态：{{ PIC_REVIEW_STATUS_MAP[record.reviewStatus] }}</div>
+          <div>审核信息：{{ record.reviewMessage }}</div>
+          <div>审核人：{{ record.reviewerId }}</div>
+          <div v-if="record.reactiveTime">
+            审核时间：{{ dayjs(record.reactiveTime).format('YYYY-MM-DD HH:mm:ss') }}
+          </div>
+        </template>
+        <template v-if="column.dataIndex === 'createTime'">
+          {{ dayjs(record.createTime).format('YYYY-MM-DD HH:mm:ss') }}
+        </template>
+        <template v-if="column.dataIndex === 'editTime'">
+          {{ dayjs(record.editTime).format('YYYY-MM-DD HH:mm:ss') }}
+        </template>
+        <template v-else-if="column.key === 'action'">
+          <a-space direction="vertical">
+            <a-button
+              v-if="record.reviewStatus !== PIC_REVIEW_STATUS_ENUM.PASS"
+              type="link"
+              @click="handleReview(record, PIC_REVIEW_STATUS_ENUM.PASS)"
+              target="_blank"
+            >
+              通过
+            </a-button>
+            <a-button
+              v-if="record.reviewStatus !== PIC_REVIEW_STATUS_ENUM.REJECT"
+              type="link"
+              danger
+              @click="handleReview(record, PIC_REVIEW_STATUS_ENUM.REJECT)"
+            >
+              拒绝
+            </a-button>
+            <a-button type="link" :href="`/add_picture?id=${record.id}`" target="_blank">
+              编辑
+            </a-button>
+            <a-button danger @click="doDelete(record.id)">删除</a-button>
+          </a-space>
+        </template>
+      </template>
+    </a-table>
+  </div>
+</template>
+<script lang="ts" setup>
+import {
+  deletePictureUsingPost,
+  doPictureReviewUsingPost,
+  listPictureByPageUsingPost,
+} from '@/api/pictureController'
+import { message } from 'ant-design-vue'
+import { computed, onMounted, reactive, ref } from 'vue'
+import dayjs from 'dayjs'
+import {
+  PIC_REVIEW_STATUS_ENUM,
+  PIC_REVIEW_STATUS_MAP,
+  PIC_REVIEW_STATUS_OPTIONS,
+} from '@/constants/picture'
+
+const columns = [
+  {
+    title: 'id',
+    dataIndex: 'id',
+    width: 80,
+  },
+  {
+    title: '图片',
+    dataIndex: 'url',
+  },
+  {
+    title: '名称',
+    dataIndex: 'name',
+  },
+  {
+    title: '简介',
+    dataIndex: 'introduction',
+    ellipsis: true,
+  },
+  {
+    title: '类型',
+    dataIndex: 'category',
+  },
+  {
+    title: '标签',
+    dataIndex: 'tags',
+  },
+  {
+    title: '图片信息',
+    dataIndex: 'picInfo',
+  },
+  {
+    title: '用户 id',
+    dataIndex: 'userId',
+    width: 80,
+  },
+  {
+    title: '审核信息',
+    dataIndex: 'reviewMessage',
+  },
+  {
+    title: '创建时间',
+    dataIndex: 'createTime',
+  },
+  {
+    title: '编辑时间',
+    dataIndex: 'editTime',
+  },
+  {
+    title: '操作',
+    key: 'action',
+  },
+]
+
+// 数据
+const dataList = ref<API.Picture[]>([])
+const total = ref(0)
+
+// 搜索条件
+const searchParams = reactive<API.PictureQueryRequest>({
+  current: 1,
+  pageSize: 10,
+  sortField: 'createTime',
+  sortOrder: 'descend',
+})
+
+// 获取数据
+const fetchData = async () => {
+  const res = await listPictureByPageUsingPost({
+    ...searchParams,
+  })
+  if (res.data.data) {
+    dataList.value = res.data.data.records ?? []
+    total.value = res.data.data.total ?? 0
+  } else {
+    message.error('获取数据失败，' + res.data.message)
+  }
+}
+
+//分页参数
+const pagination = computed(() => {
+  return {
+    total: total.value,
+    current: searchParams.current,
+    pageSize: searchParams.pageSize,
+    showSizeChanger: true,
+    showQuickJumper: true,
+    showTotal: (total) => `共 ${total} 条`,
+  }
+})
+
+//表格更新
+const doTabChange = (page: any) => {
+  searchParams.current = page.current
+  searchParams.pageSize = page.pageSize
+  fetchData()
+}
+
+//搜索
+const doSearch = () => {
+  searchParams.current = 1
+  fetchData()
+}
+//删除数据
+const doDelete = async (id: string) => {
+  if (!id) {
+    return
+  }
+  const res = await deletePictureUsingPost({ id })
+  if (res.data.code === 0) {
+    message.success('删除成功')
+    fetchData()
+  } else {
+    message.error('删除失败，' + res.data.message)
+  }
+}
+//审核图片
+const handleReview = async (record: API.Picture, reviewStatus: number) => {
+  const reviewMessage =
+    reviewStatus === PIC_REVIEW_STATUS_ENUM.PASS ? '管理员操作通过' : '管理员操作拒绝'
+  const res = await doPictureReviewUsingPost({
+    id: record.id,
+    reviewStatus,
+    reviewMessage,
+  })
+  if (res.data.code === 0) {
+    message.success('审核操作成功')
+    // 重新获取列表
+    fetchData()
+  } else {
+    message.error('审核操作失败，' + res.data.message)
+  }
+}
+
+// 页面加载时请求一次
+onMounted(() => {
+  fetchData()
+})
+</script>
